f26114b4e72c294f0633233cbf8250b7d4b89d0a
kauezatarin <kauezatarin@hotmail.com>
2016-06-03 23:32:14 -0300

    Criado funções base, e configurações basicas

---
+++ Comunicador_Serial/Main.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Comunicador_Serial
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btConectar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btEnviar = new System.Windows.Forms.Button();
+            this.textBoxEnviar = new System.Windows.Forms.TextBox();
+            this.textBoxReceber = new System.Windows.Forms.TextBox();
+            this.timerCOM = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.scanButton = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.dicaLiveScan = new System.Windows.Forms.ToolTip(this.components);
+            this.dicaCleanOnSend = new System.Windows.Forms.ToolTip(this.components);
+            this.btClearText = new System.Windows.Forms.Button();
+            this.dicaClearBox = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // btConectar
+            // 
+            this.btConectar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btConectar.Location = new System.Drawing.Point(13, 13);
+            this.btConectar.Name = "btConectar";
+            this.btConectar.Size = new System.Drawing.Size(75, 23);
+            this.btConectar.TabIndex = 0;
+            this.btConectar.Text = "Conectar";
+            this.btConectar.UseVisualStyleBackColor = true;
+            this.btConectar.Click += new System.EventHandler(this.btConectar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(95, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btEnviar
+            // 
+            this.btEnviar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btEnviar.Location = new System.Drawing.Point(13, 42);
+            this.btEnviar.Name = "btEnviar";
+            this.btEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btEnviar.TabIndex = 2;
+            this.btEnviar.Text = "Enviar";
+            this.btEnviar.UseVisualStyleBackColor = true;
+            this.btEnviar.Click += new System.EventHandler(this.btEnviar_Click);
+            // 
+            // textBoxEnviar
+            // 
+            this.textBoxEnviar.Location = new System.Drawing.Point(95, 44);
+            this.textBoxEnviar.Name = "textBoxEnviar";
+            this.textBoxEnviar.Size = new System.Drawing.Size(633, 20);
+            this.textBoxEnviar.TabIndex = 3;
+            this.textBoxEnviar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxEnviar_KeyPress);
+            // 
+            // textBoxReceber
+            // 
+            this.textBoxReceber.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBoxReceber.Location = new System.Drawing.Point(13, 86);
+            this.textBoxReceber.Multiline = true;
+            this.textBoxReceber.Name = "textBoxReceber";
+            this.textBoxReceber.ReadOnly = true;
+            this.textBoxReceber.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxReceber.Size = new System.Drawing.Size(715, 163);
+            this.textBoxReceber.TabIndex = 4;
+            // 
+            // timerCOM
+            // 
+            this.timerCOM.Interval = 1000;
+            this.timerCOM.Tick += new System.EventHandler(this.timerCOM_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(240, 14);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(78, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(324, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Baud";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(490, 18);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Live Scan";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // scanButton
+            // 
+            this.scanButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.scanButton.Location = new System.Drawing.Point(374, 13);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(75, 23);
+            this.scanButton.TabIndex = 8;
+            this.scanButton.Text = "Scan";
+            this.scanButton.UseVisualStyleBackColor = true;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(571, 17);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(104, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "Limpar ao enviar";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // btClearText
+            // 
+            this.btClearText.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btClearText.Image = global::Comunicador_Serial.Properties.Resources.clear;
+            this.btClearText.Location = new System.Drawing.Point(696, 9);
+            this.btClearText.Name = "btClearText";
+            this.btClearText.Size = new System.Drawing.Size(32, 32);
+            this.btClearText.TabIndex = 10;
+            this.btClearText.UseVisualStyleBackColor = true;
+            this.btClearText.Click += new System.EventHandler(this.btClearText_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 261);
+            this.Controls.Add(this.btClearText);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.scanButton);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBoxReceber);
+            this.Controls.Add(this.textBoxEnviar);
+            this.Controls.Add(this.btEnviar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btConectar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arduino Serial Comunicator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConectar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btEnviar;
+        private System.Windows.Forms.TextBox textBoxEnviar;
+        private System.Windows.Forms.TextBox textBoxReceber;
+        private System.Windows.Forms.Timer timerCOM;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button scanButton;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.ToolTip dicaLiveScan;
+        private System.Windows.Forms.ToolTip dicaCleanOnSend;
+        private System.Windows.Forms.Button btClearText;
+        private System.Windows.Forms.ToolTip dicaClearBox;
+    }
+}
+
---
+++ Comunicador_Serial/Main.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports; // necessário para ter acesso as portas
+
+namespace Comunicador_Serial
+{
+    public partial class Main : Form
+    {
+        string RxString;
+        public Main()//não modificar
+        {
+            InitializeComponent();
+        }
+
+        /*-------Funções de formulario--------*/
+        private void Form1_Load(object sender, EventArgs e)//ao iniciar o programa
+        {
+            textBoxReceber.AppendText("Inicializando...");
+
+            configurate();//le as configurações
+            dicas();//inicializa as dicas
+
+            if (timerCOM.Enabled == true)
+                textBoxReceber.AppendText(Environment.NewLine+"Live scan ativado...");
+            else
+                textBoxReceber.AppendText(Environment.NewLine + "Live scaner desativado...");
+
+            textBoxReceber.AppendText(Environment.NewLine +"Bem Vindo!");
+
+            comboBox2.Items.Add("300");
+            comboBox2.Items.Add("1200");
+            comboBox2.Items.Add("2400");
+            comboBox2.Items.Add("4800");
+            comboBox2.Items.Add("9600");
+            comboBox2.Items.Add("19200");
+            comboBox2.Items.Add("38400");
+            comboBox2.Items.Add("57600");
+            comboBox2.Items.Add("74880");
+            comboBox2.Items.Add("115200");
+            comboBox2.Items.Add("230400");
+            comboBox2.Items.Add("250000");
+
+            comboBox2.SelectedIndex = 4;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)//ao fechar o programa
+        {
+            if (serialPort1.IsOpen == true)  // se porta aberta
+                serialPort1.Close();         //fecha a porta
+        }
+
+        /*-------Funções de chamada---------*/
+        private void atualizaListaCOMs()//atualiza as portas COM disponiveis
+        {
+            int i;
+            bool quantDiferente; //flag para sinalizar que a quantidade de portas mudou
+
+            i = 0;
+            quantDiferente = false;
+
+            //se a quantidade de portas mudou
+            if (comboBox1.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    if (comboBox1.Items[i++].Equals(s) == false)
+                    {
+                        quantDiferente = true; 
+                    }
+                }
+            }
+            else
+            {
+                quantDiferente = true;
+            }
+
+            //Se não foi detectado diferença
+            if (quantDiferente == false)
+            {
+                return;                     //retorna
+            }
+
+            //limpa comboBox
+            comboBox1.Items.Clear();
+            int counter = 0;
+
+            //adiciona todas as COM diponíveis na lista
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+                counter++;
+            }
+
+            //seleciona a primeira posição da lista
+            if(counter != 0)//se a lista não estiver vazia
+                comboBox1.SelectedIndex = 0;
+            else//se a lista estiver vazia
+                comboBox1.Items.Add("");
+                comboBox1.SelectedIndex = 0;
+
+        }
+
+        private void enviarComandos()//envia os comandos pela serial
+        {
+            if (serialPort1.IsOpen == true)          //porta está aberta
+            {
+                if(textBoxEnviar.Text != "")//verifica se a textbox está vazia
+                {
+                    serialPort1.Write(textBoxEnviar.Text);  //envia o texto presente no textbox Enviar
+                    textBoxReceber.AppendText(Environment.NewLine + "Comando ''" + textBoxEnviar.Text + "'' enviado.");
+                }
+                
+                if(Properties.Settings.Default.clear_on_send == true)//verifica a opção clear on send
+                {
+                    textBoxEnviar.Text = "";
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Conecte-se primeiro!", "Erro");
+            }
+        }
+
+        private void configurate()//le as configurações
+        {
+            //le a configuração live scan
+            timerCOM.Enabled = true;
+
+            if (Properties.Settings.Default.live_Scan == true)
+            {
+                checkBox1.Checked = true;
+                scanButton.Enabled = false;//desativa o botão scan
+            }
+            else
+            {
+                checkBox1.Checked = false;
+                scanButton.Enabled = true;//ativa o botão scan
+            }
+
+            //le a configuração clear on sent
+            if (Properties.Settings.Default.clear_on_send == true)
+            {
+                checkBox2.Checked = true;
+                Properties.Settings.Default.clear_on_send = true;
+                Properties.Settings.Default.Save();//para garantir o carregamento correto da opção
+            }
+            else
+            {
+                checkBox2.Checked = false;
+            }
+        }
+
+        private void dicas()//exibe as caixas de dicas
+        {
+            /*-----dica live scan-----*/
+            System.Windows.Forms.ToolTip dicaLiveScan = new System.Windows.Forms.ToolTip();
+            dicaLiveScan.SetToolTip(this.checkBox1, "Busca automatica de Portas COM disponiveis");
+            /*-----dica clean on send------*/
+            System.Windows.Forms.ToolTip dicaCleanSend = new System.Windows.Forms.ToolTip();
+            dicaLiveScan.SetToolTip(this.checkBox2, "Apagar comando após o envio");
+            /*-----dica clear button------*/
+            System.Windows.Forms.ToolTip dicaClearBox = new System.Windows.Forms.ToolTip();
+            dicaLiveScan.SetToolTip(this.btClearText, "Limpar Log");
+        }
+
+        /*-------Botões---------------------*/
+        private void btConectar_Click(object sender, EventArgs e)//botão de conectar
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                try
+                {
+                    serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Items[comboBox2.SelectedIndex]);
+                    serialPort1.Open();
+                }
+                catch
+                {
+                    return;
+
+                }
+                if (serialPort1.IsOpen)
+                {
+                    btConectar.Text = "Desconectar";
+                    comboBox1.Enabled = false;//desativa o combobox COM port
+                    comboBox2.Enabled = false;//desativa o combobox baud rate
+                    scanButton.Enabled = false;//desativa o botão scan
+
+                }
+            }
+            else
+            {
+
+                try
+                {
+                    serialPort1.Close();
+                    comboBox1.Enabled = true;//ativa o combobox COM port
+                    comboBox2.Enabled = true;//ativa o combobox baud rate
+
+                    if (checkBox1.Checked == false)
+                        scanButton.Enabled = true;//ativa o botão scan
+
+                    btConectar.Text = "Conectar";
+                }
+                catch
+                {
+                    return;
+                }
+
+            }
+        }
+
+        private void btEnviar_Click(object sender, EventArgs e)//botão de enviar
+        {
+            enviarComandos();//envia comandos
+        }
+
+        private void scanButton_Click(object sender, EventArgs e)//botão Scan
+        {
+            atualizaListaCOMs();
+        }
+
+        private void btClearText_Click(object sender, EventArgs e)//limpa a caixa de recebidos
+        {
+            textBoxReceber.Text = "";
+        }
+
+        /*--------checkbox------------------*/
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)//live scan on/off
+        {
+            if (timerCOM.Enabled == true)
+            {
+                timerCOM.Enabled = false;
+                Properties.Settings.Default.live_Scan = false;//altera a configuração na sessão
+                Properties.Settings.Default.Save();//salva as configurações
+                scanButton.Enabled = true;//ativa o botão scan
+            }
+            else
+            {
+                timerCOM.Enabled = true;
+                Properties.Settings.Default.live_Scan = true;//altera a configuração do live scan
+                Properties.Settings.Default.Save();//salva as configurações
+                scanButton.Enabled = false;//desativa o botão scan
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)//clear on sent on/off
+        {
+            if(Properties.Settings.Default.clear_on_send == true)
+            {
+                Properties.Settings.Default.clear_on_send = false;//altera a configuração do clear on send
+                Properties.Settings.Default.Save();//salva as configurações
+            }
+            else
+            {
+                Properties.Settings.Default.clear_on_send = true;//altera a configuração do clear on send
+                Properties.Settings.Default.Save();//salva as configurações
+            }
+        }
+
+        /*--------Serial Port, receber/enviar/tratar dados---------*/
+        private void timerCOM_Tick(object sender, EventArgs e)//a cada x milisegundos executa a função
+        {
+            atualizaListaCOMs();
+        }
+                
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)//serial port
+        {
+            RxString = serialPort1.ReadExisting();              //le o dado disponível na serial
+            this.Invoke(new EventHandler(trataDadoRecebido));   //chama outra thread para escrever o dado no text box
+        }
+
+        private void trataDadoRecebido(object sender, EventArgs e)//printa o dado recebido do dispositivo no console
+        {
+            textBoxReceber.AppendText(Environment.NewLine+"Recebido: "+ RxString);
+        }
+
+        private void textBoxEnviar_KeyPress(object sender, KeyPressEventArgs e)//ao Precionar ENTER na envia os comandos da textbox1
+        {
+            if (e.KeyChar == 13)//se precionado enter
+            {
+                e.Handled = true;//retira o barulho chato ao precionar enter
+                enviarComandos();//envia os comandos
+            }
+        }
+    }
+}
